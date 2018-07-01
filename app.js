9192a71d1dc035364b53bfce190e07cd3c846f20
rsacramento18 <ricardo_sacramento@outlook.com>
2018-07-01 22:13:40 +0100

    added a query and new router

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const app = express();
+
+const QUERY_SOCIOS = 'SELECT * FROM SOCIOS';
+
 //Create connection
 const db = mysql.createConnection({
     host : 'fop.com.pt',
@@ -19,7 +23,22 @@ db.connect((err) =>{
     console.log('Mysql Connected');
 });
 
-const app = express();
+app.get('/', (req, res) => {
+    console.log('go to /products')
+});
+app.get('/products', (req, res) => {
+    db.query(QUERY_SOCIOS, (err, results) =>{
+        if(err){
+            return res.send(err);
+        }
+        else{
+            return res.json({
+                data: results
+            })
+        }
+    });
+});
+
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
