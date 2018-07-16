e76a4f881d195dd232d9bd188a878da15f91071a
rsacramento18 <ricardo_sacramento@outlook.com>
2018-07-16 22:10:58 +0100

    Added more routes into associates

--- server/src/associates.js
+++ server/src/associates.js
@@ -2,11 +2,20 @@ const express = require('express')
 const router = express.Router()
 const db = require('./db')
 
-const QUERY_SOCIOS = 'SELECT * FROM socios'
-
 // Get all associates
-router.get('/all', (req, res) => {
-  db.query(QUERY_SOCIOS, (err, results) => {
+// - - - - - Can be with club(club)
+router.get('/all/fop', (req, res) => {
+  let clube = req.param('club')
+  let sql= ""
+
+  if(clube !== undefined) {
+    sql= `SELECT stam, nome FROM socios INNER JOIN socios_clubes ON socios.stam=socios_clubes.stam WHERE clube = ${clube}`
+  }
+  else {
+    sql= `SELECT stam, nome FROM socios`
+  }
+
+  db.query(sql, (err, results) => {
     if (err) {
       return res.send(err)
     } else {
@@ -18,10 +27,48 @@ router.get('/all', (req, res) => {
 })
 
 // Get single associate
+// - - - - - Required id(stam) but not club(clube)
 router.get('/single', (req, res) => {
-  var stam = req.param('id')
-  var clube = req.param('club')
-  let sql = `SELECT * FROM socios INNER JOIN socios_clubes ON socios.stam=socios_clubes.stam WHERE socios.stam = ${stam} AND clube = ${clube}`
+  let stam = req.param('id')
+  let clube = req.param('club')
+  let sql= ""
+
+  if(clube !== undefined) {
+     sql = `SELECT * FROM socios INNER JOIN socios_clubes ON socios.stam=socios_clubes.stam WHERE socios.stam = ${stam} AND clube = ${clube}`
+  }
+  else{
+     sql = `SELECT * FROM socios WHERE socios.stam = ${stam}`
+  }
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.send(err)
+    } else {
+      return res.json({
+        data: results
+      })
+    }
+  })
+})
+
+router.get('/all/fonp', (req, res) => {
+  sql= `SELECT * FROM contasociofonp`
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.send(err)
+    } else {
+      return res.json({
+        data: results
+      })
+    }
+  })
+})
+
+
+router.get('/all/international', (req, res) => {
+  sql= `SELECT * FROM contasestrangeiro`
+
   db.query(sql, (err, results) => {
     if (err) {
       return res.send(err)
